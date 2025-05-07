11839bba51cd4843cb774c3525b2fb74e635a449
Soybond1 <estebanconrado91@gmail.com>
2025-05-07 17:48:35 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Music-On
+
+# Music-On Prototype
+
+App interactiva para crear ritmos musicales con una interfaz divertida y sencilla.
+
+## Instalación
+
+```bash
+npm install
+npm run dev
+```
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MusicOnApp from './MusicOnApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MusicOnApp />
+  </React.StrictMode>
+)
